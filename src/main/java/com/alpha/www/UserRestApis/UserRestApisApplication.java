dc18786462145e20a8a00ad0f3880b246054c729
Nick <masihnishit@gmail.com>
2025-01-25 16:20:02 +0530

    new spring boot project

---
+++ src/main/java/com/alpha/www/UserRestApis/UserRestApisApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.www.UserRestApis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRestApisApplication.class, args);
+	}
+
+}
