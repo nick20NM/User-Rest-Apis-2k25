f061e75d12b52cd1c3946dbce2bd44ae7e600969
Nick <masihnishit@gmail.com>
2025-01-29 19:46:52 +0530

    Exception handler added for UserController class with custom error response

--- src/main/java/com/alpha/www/UserRestApis/controller/UserController.java
+++ src/main/java/com/alpha/www/UserRestApis/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.alpha.www.UserRestApis.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +14,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 
 import com.alpha.www.UserRestApis.dto.UserDto;
 import com.alpha.www.UserRestApis.entity.User;
+import com.alpha.www.UserRestApis.exception.ErrorDetails;
+import com.alpha.www.UserRestApis.exception.ResourceNotFoundException;
 import com.alpha.www.UserRestApis.service.UserService;
 
 import lombok.AllArgsConstructor;
@@ -56,5 +61,18 @@ public class UserController {
 	public ResponseEntity<String> deleteUser(@PathVariable Long id){
 		userService.deleteUser(id);
 		return ResponseEntity.ok("User successfully deleted with id: " + id);
+	}
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
+			ResourceNotFoundException exception, 
+			WebRequest webRequest){
+		ErrorDetails errorDetails = new ErrorDetails(
+				LocalDateTime.now(), 
+				exception.getMessage(), 
+				webRequest.getDescription(false), 
+				"USER_NOT_FOUND");
+		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+		
 	}
 }
