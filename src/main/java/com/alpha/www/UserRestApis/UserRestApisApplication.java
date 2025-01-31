f96c857d7eb10c910732d55cdf71cc8e186c486a
Nick <masihnishit@gmail.com>
2025-01-31 11:38:58 +0530

    Define General API Information using Annotations

--- src/main/java/com/alpha/www/UserRestApis/UserRestApisApplication.java
+++ src/main/java/com/alpha/www/UserRestApis/UserRestApisApplication.java
@@ -5,7 +5,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot REST API Documentation", 
+				description = "Spring Boot REST API Documentation", 
+				version = "v1.0", 
+				contact = @Contact(
+						name = "Nishit", 
+						email = "nishitmasih32@gmail.com", 
+						url = "https://www.linkedin.com/in/nishit-masih/"
+						), 
+				license = @License(
+						name = "Apache 2.0", 
+						url = "https://www.linkedin.com/in/nishit-masih/"
+						)
+				), 
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot User Management Documentation", 
+				url = "https://www.linkedin.com/in/nishit-masih/"
+				)
+		)
 public class UserRestApisApplication {
 	
 	@Bean
